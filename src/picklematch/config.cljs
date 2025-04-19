13bca138533268f474d12e863b11acda7c81fd19
Vladimir Rurik <vladimir.rurik@gmail.com>
2025-04-20 01:59:21 +0300

    feat: Add Firebase configuration and update build scripts to use dotenv

---
+++ src/picklematch/config.cljs
@@ -0,0 +1,8 @@
+(ns picklematch.config)
+
+(goog-define FIREBASE_API_KEY              "")
+(goog-define FIREBASE_AUTH_DOMAIN          "")
+(goog-define FIREBASE_PROJECT_ID           "")
+(goog-define FIREBASE_STORAGE_BUCKET       "")
+(goog-define FIREBASE_MESSAGING_SENDER_ID  "")
+(goog-define FIREBASE_APP_ID               "")
